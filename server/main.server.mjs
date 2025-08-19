06446597e9ca23c16ad08d1d658d3b38bd81f7b0
MarieSeclen <mariestephanieseclensanchez@gmail.com>
2025-08-19 16:55:42 -0500

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-WCY3UUJE.mjs";import"./chunk-RJ7D32H2.mjs";import"./chunk-SDOAP7XV.mjs";import"./chunk-DV4W5GVV.mjs";import"./chunk-H5NLI47V.mjs";import"./chunk-NGK3KENG.mjs";import"./chunk-SU24D2KP.mjs";import"./chunk-VMT4DUF3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
